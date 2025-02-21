1a6b5e3b572dc0f237f529811097d0ee204a46a1
NurAfifah1234 <nur.afifah@student.alhazen.academy>
2025-02-21 14:15:00 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from mathematics.circle.area import circle_area
+from mathematics.square.area import square_area
+from mathematics.triangle.area import triangle_area
+
+print(circle_area(10))
+print(square_area(15))
+print(triangle_area(10, 15))
