cd60ac1810216f6a3e707d79d085781cdc5978cb
NurAfifah1234 <nur.afifah@student.alhazen.academy>
2025-04-25 00:17:59 -0700

    Add files via upload

---
+++ pip.py
@@ -0,0 +1,7 @@
+from stringcolor import *
+
+print(cs("selamat datang", "blue", "white"))
+print(bold("selamat datang"))
+print(underline("selamat datang"))
+print(underline("selamat datang").bold())
+print(cs("selamat datang", "blue", "yellow").underline())
